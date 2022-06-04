825f886b6a2623a50af168d9742cce5f845daea1
Dean Hunter <jeeka7@gmail.com>
2022-06-05 00:17:07 +0530

    Update guessword.py

--- guessword.py
+++ guessword.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.write("Write a 4 letter english word")
 
 import enchant
-st.write(help(enchant))
+dict = enchant.Dict("en_US")
+word = input("Enter the word: ")
+print(dict.check(word))
